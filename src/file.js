cddd74c6c5b84f46cd26556bbca6402091e9a419
Dheeraj Pundir <44019618+dheeraj-p@users.noreply.github.com>
2018-12-04 10:58:08 +0530

    Add function getLines() to get lines of a file

--- src/file.js
+++ src/file.js
@@ -1,6 +1,11 @@
 const newFile = function(){
   let file = {name: '', contents: ''};
+  file.getLines = getLines.bind(file);
   return file;
+}
+
+const getLines = function(){
+  return this.contents.split("\n");
 }
 
 exports.newFile = newFile;
--- test/file_test.js
+++ test/file_test.js
@@ -1,9 +1,23 @@
 const assert = require('assert');
 const {newFile} = require('../src/file.js');
 
-describe('newFile', function(){
-  it("should return an new file object", function(){
-    let expectedOutput = {name: '', contents: ''};
-    assert.deepEqual(newFile(), expectedOutput);
+describe('file : getLines', function(){
+  it("should return empty array when an empty file is given", function(){
+    let file = newFile();
+    assert.deepEqual(file.getLines(), ['']);
+  });
+
+  it("should return one line when an one line file is given", function(){
+    let file = newFile();
+    file.contents = "this is first line";
+    assert.deepEqual(file.getLines(), ['this is first line']);
+  });
+
+  it("should return lines of file when a multiple lines file is given", function(){
+    let file = newFile();
+    file.contents = "this is first line\n" + 
+                    "this is second line\n" +
+                    "this is third line";
+    assert.deepEqual(file.getLines(), ['this is first line', "this is second line", "this is third line"]);
   });
 });
