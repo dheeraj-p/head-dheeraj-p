b147b8a00629d7184595d3a7575288d5e69aabfb
Dheeraj Pundir <44019618+dheeraj-p@users.noreply.github.com>
2018-12-05 12:15:09 +0530

    Upgrade head.js -> Now it supports multiple files and options

--- head.js
+++ head.js
@@ -1,15 +1,17 @@
 const {
-  getLinesFromHead,
+  head,
   read
 } = require('./src/head_lib.js');
 const reader = require('fs').readFileSync;
 const {newFile} = require('./src/file.js');
+const {parseInputs} = require('./src/io.js');
 
 const main = function(){
-  let filePath = process.argv[2];
-  let fileContents = read(reader, filePath, "utf-8");
-  let file = newFile(filePath, fileContents);
-  let headContents = getLinesFromHead(file);
+  let userInputs = parseInputs(process.argv.slice(2));
+  let fileNames = userInputs.fileNames;
+  let files = fileNames.map(fileName => newFile(fileName, read(reader, fileName, "utf-8")));
+  let headData = {option: userInputs.option, optionValue: userInputs.optionValue, files};
+  let headContents = head(headData);
   console.log(headContents);
 }
 
