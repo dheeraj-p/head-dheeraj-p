3c2bb7fc68a8855a414c5bab955d5ffaaadc16b6
Dheeraj Pundir <44019618+dheeraj-p@users.noreply.github.com>
2018-12-05 10:36:13 +0530

    Refactor parseInputs() to support multiple options and files

--- src/io.js
+++ src/io.js
@@ -1,6 +1,20 @@
 const parseInputs = function(inputs){
-  let parsedInputs = {option: 'n', optionValue : 10};
-  parsedInputs.fileNames = [...inputs];
+  let option = '-n';
+  let optionValue = '10';
+  let fileNames = [...inputs];
+
+  let optionMatched = inputs[0].match(/^-(n|c)/);
+  if(optionMatched != null){
+    option = optionMatched[0];
+    optionValue = inputs[0].substr(2);
+    fileNames = inputs.slice(1);
+    if(optionValue == ''){
+      optionValue = inputs[1];
+      fileNames = inputs.slice(2);
+    }
+  }
+
+  let parsedInputs = {option, optionValue, fileNames};
   return parsedInputs;
 }
 
--- test/io_test.js
+++ test/io_test.js
@@ -3,7 +3,17 @@ const {parseInputs} = require('../src/io.js');
 
 describe("parseInputs", function(){
   it("should return inputs in form of object containing all the file names in default case", function(){
-    let expectedOutput = {option: 'n', fileNames: ['file1', 'file2'], optionValue : 10};
+    let expectedOutput = {option: '-n', fileNames: ['file1', 'file2'], optionValue : '10'};
     assert.deepEqual(parseInputs(['file1', 'file2']), expectedOutput);
+  });
+
+  it("should return inputs in form of object when option is provided with one file", function(){
+    let expectedOutput = {option: '-n', fileNames: ['file'], optionValue : '4'};
+    assert.deepEqual(parseInputs(['-n4', 'file']), expectedOutput);
+  });
+
+  it("should return inputs in form of object when option is provided with multiple files", function(){
+    let expectedOutput = {option: '-n', fileNames: ['file1', 'file2', 'file3'], optionValue : '4'};
+    assert.deepEqual(parseInputs(['-n4', 'file1', 'file2', 'file3']), expectedOutput);
   });
 });
